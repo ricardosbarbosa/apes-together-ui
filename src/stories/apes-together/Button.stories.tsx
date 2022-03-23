2f0806ae244df8240b74175c3d4cb42d7895bd43
Ricardo Barbosa <rbrico@gmail.com>
2022-03-23 11:17:16 -0300

    button secondary

--- src/stories/apes-together/Button.stories.tsx
+++ src/stories/apes-together/Button.stories.tsx
@@ -2,8 +2,9 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Button from '@mui/material/Button';
 
 import ThemeProvider from '../../components/ThemeProvider';
-import { IconButton, Stack } from '@mui/material';
-import { ArrowUpRight, Plus, Trash } from 'phosphor-react';
+import Stack from '@mui/material/Stack';
+// import IconButton from '@mui/material/IconButton';
+// import { ArrowUpRight, Plus, Trash } from 'phosphor-react';
 
 export default {
   title: 'Components/Button',
@@ -34,42 +35,39 @@ export const Primary: ComponentStory<typeof Button> = (args) => (
     <Button {...args} variant='primary' children="Disabled" disabled />
   </Stack>
 )
+export const Secondary: ComponentStory<typeof Button> = (args) => (
+  <Stack direction={'column'} spacing={5}>
+    <Button {...args} variant='secondary' children="Secondary" />
+    <Button {...args} variant='secondary' children="Hover" />
+    <Button {...args} variant='secondary' children="Focus" />
+    <Button {...args} variant='secondary' children="Secondary" disabled />
+  </Stack>
+)
 
-export const Secondary = Playground.bind({});
-Secondary.args = {
-  children: 'Secondary',
-  variant: 'secondary',
-};
-export const DisabledSecondary = Playground.bind({});
-DisabledSecondary.args = {
-  children: 'Secondary',
-  variant: 'secondary',
-  disabled: true
-};
-export const RightIcon = Playground.bind({});
-RightIcon.args = {
-  endIcon: <ArrowUpRight size={20} />,
-  variant: 'secondary',
-  children: 'secondary'
-};
+// export const RightIcon = Playground.bind({});
+// RightIcon.args = {
+//   endIcon: <ArrowUpRight size={20} />,
+//   variant: 'secondary',
+//   children: 'secondary'
+// };
 
-export const LeftIcon = Playground.bind({});
-LeftIcon.args = {
-  startIcon: <Plus size={20} />,
-  variant: 'secondary',
-  children: 'secondary'
-};
+// export const LeftIcon = Playground.bind({});
+// LeftIcon.args = {
+//   startIcon: <Plus size={20} />,
+//   variant: 'secondary',
+//   children: 'secondary'
+// };
 
-const PlaygroundIconButton: ComponentStory<typeof Button> = (args) => <IconButton {...args} />;
-PlaygroundIconButton.args = {}
+// const PlaygroundIconButton: ComponentStory<typeof Button> = (args) => <IconButton {...args} />;
+// PlaygroundIconButton.args = {}
 
-export const Icon = PlaygroundIconButton.bind({});
-Icon.args = {
-  children: <Trash />,
-};
+// export const Icon = PlaygroundIconButton.bind({});
+// Icon.args = {
+//   children: <Trash />,
+// };
 
-export const DisabledIcon = PlaygroundIconButton.bind({});
-DisabledIcon.args = {
-  children: <Trash />,
-  disabled: true
-};
+// export const DisabledIcon = PlaygroundIconButton.bind({});
+// DisabledIcon.args = {
+//   children: <Trash />,
+//   disabled: true
+// };
