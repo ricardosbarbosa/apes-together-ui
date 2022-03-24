5ed8d17b99e36eaf75e5e0fdceec13ef6d09599a
Ricardo Barbosa <rbrico@gmail.com>
2022-03-24 20:18:39 -0300

    text field and text area field

---
+++ src/stories/apes-together/TextAreaField.stories.tsx
@@ -0,0 +1,31 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { TextField, Stack } from '@mui/material';
+import ThemeProvider from '../../components/ThemeProvider/ThemeProvider';
+
+export default {
+  title: 'Components/TextAreaField',
+  component: TextField,
+
+  argTypes: {},
+  decorators: [
+    (Story) => (
+      <ThemeProvider>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+} as ComponentMeta<typeof TextField>;
+
+export const TextAreaField: ComponentStory<typeof TextField> = () => {
+  return (
+    <Stack direction={'column'} spacing={5} alignItems="flex-start">
+      <TextField multiline rows={5} defaultValue="Textarea" style={{ width: '800px', height: '156px' }} />
+      <TextField multiline rows={5} placeholder='Textarea' style={{ width: '800px', height: '156px' }} />
+      <TextField multiline rows={5} defaultValue="Textarea" style={{ width: '800px', height: '156px' }} />
+      <TextField multiline rows={5} defaultValue="Textarea" style={{ width: '800px', height: '156px' }} />
+      <TextField multiline rows={5} defaultValue="Textarea" disabled style={{ width: '800px', height: '156px' }} />
+      <TextField multiline rows={5} defaultValue="Textarea" error style={{ width: '800px', height: '156px' }} />
+      <TextField multiline rows={5} defaultValue="Textarea" error style={{ width: '800px', height: '156px' }} />
+    </Stack>
+  )
+}
