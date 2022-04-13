c2290c4b5f0e0a1f5bf5e0a6392d0651e07aa59e
TSHIOKUFILA MAKASHIENY Shekinah <majorsonshekinah@gmail.com>
2022-04-13 22:46:19 +0200

    [MC-31] Button Component migrated to Tailwind Css (#25)
    * Tailwindcss Installed
    
    * Tailwind configuration
    
    * Global css for tailwind
    
    * Applying colour to the svg on mouse over
    
    * Avoiding collusion between the Button component and @mui/material
    
    * Button Component Implemented
    
    * Exporting the Button Component
    
    * Button.stories updated for the current Button

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          pleasures: '#7E4A9B',
+          hover: '#612B80',
+        },
+        creamy: {
+          coral : '#E2778A'
+        },
+        green: {
+          serengeti: '#78CB8F'
+        },
+        black: {
+          off: '#303030'
+        },
+        argent: '#757575',
+        palladium: '#B1B1B1',
+        kingly: {
+          cloud : '#DFDFDF'
+        },
+        bleached: {
+          silk: '#F2F2F2'
+        },
+      },
+      fontFamily: {
+        AktivGrotesk: ['aktiv-grotesk']
+      }
+    },
+  },
+  plugins: [],
+}
